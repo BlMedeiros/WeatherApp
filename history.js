b80066e5b6cc5fa6c061f47debade17470c203fd
BlMedeiros <165826045+BlMedeiros@users.noreply.github.com>
2026-01-02 23:27:49 -0300

    feat: inicia a estruturação do histórico de pesquisas

---
+++ history.js
@@ -0,0 +1,17 @@
+search = {
+        city_name: name
+}
+
+function addHistory(name) {
+
+    search.setItem(name)
+    
+    localStorage.setItem("search",JSON.stringify(search))
+
+}
+
+function getHistory() {
+    let getSearchHistory = localStorage.getItem("search")
+
+    let searchObject = JSON.parse(getSearchHistory)
+}
